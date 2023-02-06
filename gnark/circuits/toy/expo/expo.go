4867488f1055019ca8ecf5c6fa590e4fa1102714
tumberger <jens.ernstberger@tum.de>
2023-02-06 14:23:39 +0100

    Add tests for expo toy circuit gnark

--- gnark/circuits/toy/expo/expo.go
+++ gnark/circuits/toy/expo/expo.go
@@ -9,10 +9,11 @@ type BenchCircuit struct {
 	N int
 }
 
+// Circuit defines a an exponentiation for a frontend variable with itself
 func (circuit *BenchCircuit) Define(api frontend.API) error {
-	for i := 0; i < circuit.N; i++ {
+	for i := 0; i <= circuit.N; i++ {
 		circuit.X = api.Mul(circuit.X, circuit.X)
 	}
-	api.AssertIsEqual(circuit.X, circuit.Y)
+	api.AssertIsEqual(circuit.Y, circuit.X)
 	return nil
 }
