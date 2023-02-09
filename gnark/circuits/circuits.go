afc8ec55c51bbcfbe3533a6a46be4712a67e769d
tumberger <jens.ernstberger@tum.de>
2023-02-09 23:46:49 +0100

    Add remaining toy examples + Mimc

--- gnark/circuits/circuits.go
+++ gnark/circuits/circuits.go
@@ -3,6 +3,7 @@ package circuits
 import (
 	"github.com/consensys/gnark-crypto/ecc"
 	bls12377fr "github.com/consensys/gnark-crypto/ecc/bls12-377/fr"
+
 	bls12381fr "github.com/consensys/gnark-crypto/ecc/bls12-381/fr"
 	bls24315fr "github.com/consensys/gnark-crypto/ecc/bls24-315/fr"
 	bn254fr "github.com/consensys/gnark-crypto/ecc/bn254/fr"
@@ -10,8 +11,10 @@ import (
 	bw6761fr "github.com/consensys/gnark-crypto/ecc/bw6-761/fr"
 	"github.com/consensys/gnark/backend/witness"
 	"github.com/consensys/gnark/frontend"
+	"github.com/tumberger/zk-compilers/gnark/circuits/prf/mimc"
 	"github.com/tumberger/zk-compilers/gnark/circuits/toy/cubic"
 	"github.com/tumberger/zk-compilers/gnark/circuits/toy/expo"
+	"github.com/tumberger/zk-compilers/gnark/circuits/toy/exponentiate"
 )
 
 var BenchCircuits map[string]BenchCircuit
@@ -28,8 +31,11 @@ type TemplateCircuit struct {
 
 func init() {
 	BenchCircuits = make(map[string]BenchCircuit)
-	BenchCircuits["expo"] = &defaultCircuit{}
 	BenchCircuits["cubic"] = &defaultCircuit{}
+	BenchCircuits["expo"] = &defaultCircuit{}
+	BenchCircuits["exponentiate"] = &defaultCircuit{}
+
+	BenchCircuits["mimc"] = &defaultCircuit{}
 }
 
 func preCalc(size int, curveID ecc.ID) interface{} {
@@ -97,10 +103,14 @@ type defaultCircuit struct {
 
 func (d *defaultCircuit) Circuit(size int, name string) frontend.Circuit {
 	switch name {
-	case "expo":
-		return &expo.BenchCircuit{N: size}
 	case "cubic":
 		return &cubic.CubicCircuit{}
+	case "expo":
+		return &expo.BenchCircuit{N: size}
+	case "exponentiate":
+		return &exponentiate.ExponentiateCircuit{}
+	case "mimc":
+		return &mimc.MimcCircuit{}
 	default:
 		panic("not implemented")
 	}
@@ -109,6 +119,16 @@ func (d *defaultCircuit) Circuit(size int, name string) frontend.Circuit {
 func (d *defaultCircuit) Witness(size int, curveID ecc.ID, name string) *witness.Witness {
 
 	switch name {
+	case "cubic":
+		witness := cubic.CubicCircuit{}
+		witness.X = (3)
+		witness.Y = (35)
+
+		w, err := frontend.NewWitness(&witness, curveID)
+		if err != nil {
+			panic(err)
+		}
+		return w
 	case "expo":
 		witness := expo.BenchCircuit{N: size}
 		witness.X = (2)
@@ -119,10 +139,21 @@ func (d *defaultCircuit) Witness(size int, curveID ecc.ID, name string) *witness
 			panic(err)
 		}
 		return w
-	case "cubic":
-		witness := cubic.CubicCircuit{}
-		witness.X = (3)
-		witness.Y = (35)
+	case "exponentiate":
+		witness := exponentiate.ExponentiateCircuit{}
+		witness.X = (2)
+		witness.E = (12)
+		witness.Y = (4096)
+
+		w, err := frontend.NewWitness(&witness, curveID)
+		if err != nil {
+			panic(err)
+		}
+		return w
+	case "mimc":
+		witness := mimc.MimcCircuit{}
+		witness.PreImage = ("16130099170765464552823636852555369511329944820189892919423002775646948828469")
+		witness.Hash = ("8674594860895598770446879254410848023850744751986836044725552747672873438975")
 
 		w, err := frontend.NewWitness(&witness, curveID)
 		if err != nil {
