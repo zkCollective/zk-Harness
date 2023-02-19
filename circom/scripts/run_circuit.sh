0991ad742b9d32b1f6bbc642ddd8fc0601c1f19f
Stefanos Chaliasos <stefanoshaliassos@gmail.com>
2023-02-19 13:59:36 +0000

    Merge setup and ExportVerificationKey phases into a single phase in circom
    Add various TODOs and comments

--- circom/scripts/run_circuit.sh
+++ circom/scripts/run_circuit.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #
 # Compile, setup, prove, and verify a proof for a circom circuit.
-# #TODO add C++ witness generation support
+# TODO add C++ witness generation support
+# TODO try add PLONK and FFLONKsupport
 
 if [ $# -lt 3 ]; then
     echo $0: usage: run_circuit.sh circuit.circom input.json powersOfTau.ptau results.csv tmp
@@ -27,9 +28,13 @@ echo ">>>Step 1: compiling the circuit" && \
 /usr/bin/time -h -l -o ${TMP}/compiler_times.txt circom ${CIRCUIT} --r1cs --wasm --sym --c --output ${TMP} | tee ${TMP}/circom_output && \
 echo ">>>Step 2: generating the witness JS" && \
 /usr/bin/time -h -l -o ${TMP}/witness_times.txt node  ${TMP}/${CIRCUIT_NAME}_js/generate_witness.js ${TMP}/${CIRCUIT_NAME}_js/${CIRCUIT_NAME}.wasm ${INPUT} ${TMP}/witness.wtns && \
+# We only care about phase 2 which is circuit-specific
+# .zkey file that will contain the proving and verification keys together with 
+# all phase 2 contributions.
 echo ">>>Step 3: Setup" && \
 /usr/bin/time -h -l -o ${TMP}/setup_times.txt snarkjs groth16 setup ${TMP}/${CIRCUIT_NAME}.r1cs ${TAU} ${TMP}/${CIRCUIT_NAME}_0.zkey && \
-## We could contribute here using: snarkjs zkey contribute multiplier2_0000.zkey multiplier2_0001.zkey --name="1st Contributor Name" -v
+# TODO Should we contribute here?
+# We could contribute here using: snarkjs zkey contribute ${TMP}/${CIRCUIT_NAME}_0.zkey ${TMP}/${CIRCUIT_NAME}_1.zkey --name="1st Contributor Name" -v
 echo ">>>Step 4: Export verification key" && \
 /usr/bin/time -h -l -o ${TMP}/export_times.txt snarkjs zkey export verificationkey ${TMP}/${CIRCUIT_NAME}_0.zkey ${TMP}/verification_key.json && \
 echo ">>>Step 5: Prove" && \
@@ -64,10 +69,44 @@ get_time_results() {
 get_phase_stats() {
     phase=$1
     phaseTimeFile=$2
+    phaseTimeFileToMerge=$3
+
     ramtime="$(get_time_results $phaseTimeFile)"
+    # TODO Node uses 1 single thread in one core. Nevertheless, snarkjs
+    # (and the underlying library ffjavascript)
+    # use workers to perfrom operations, hence it isn't actually single threaded.
+    # We could instrument snarkjs so it uses only a single worker.
+    # For circom compiler, again we could potentially enforce single core and
+    # thread execution if we instrument it.
+    # Finally, we might need to do the same for the witness generator.
     physical=1
     virtual=1
-    echo "circom,circuit,groth16,bn128,$CIRCUIT_NAME,$INPUT,$phase,$nbConstraints,$nbPrivateInputSignals,$nbPublicInputSignals,$ramtime,$physical,$virtual,$PROC"
+    # Proof size in bytes
+    if [ $phase == "prove" ]; then 
+        proofSize=$(stat -f%z ${TMP}/proof.json)
+    else
+        proofSize=""
+    fi
+    # If phaseTimeFileToMerge is not empty then merge its results with phaseTimeFile 
+    if [ ! -z "$phaseTimeFileToMerge" ]; then
+        ramtimeToMerge="$(get_time_results $phaseTimeFileToMerge)"
+        echo $ramtimeToMerge > fff
+        ramInitial=$(echo $ramtime | cut -d ',' -f1)
+        ramToMerge=$(echo $ramtimeToMerge | cut -d ',' -f1)
+        timeInitial=$(echo $ramtime | cut -d ',' -f2)
+        timeToMerge=$(echo $ramtimeToMerge | cut -d ',' -f2)
+        if [ "$ramInitial" -gt "$ramToMerge" ]; then
+            ramFinal=$ramInitial
+        else
+            ramFinal=$ramToMerge
+        fi
+        timeFinal=$(($timeInitial + $timeToMerge))
+        ramtimeFinal="${ramFinal},${timeFinal}"
+    else
+        ramtimeFinal=$ramtime
+    fi
+    # TODO add proof size before phyical cores
+    echo "circom,circuit,groth16,bn128,$CIRCUIT_NAME,$INPUT,$phase,$nbConstraints,$nbPrivateInputSignals,$nbPublicInputSignals,$ramtimeFinal,$physical,$virtual,$PROC"
 
 }
 
@@ -84,22 +123,21 @@ if [ ! -z "$RES" ]; then
     nbPublicOutputSignals=$(grep "^public outputs" ${TMP}/circom_output | cut -d ":" -f2 | xargs)
     nbWires=$(grep "^wires" ${TMP}/circom_output | cut -d ":" -f2 | xargs)
     nbLabels=$(grep "^labels" ${TMP}/circom_output | cut -d ":" -f2 | xargs)
-    declare -a stages=("Compiler" 
-                       "WitnessGeneration" 
-                       "Setup"
-                       "ExportVerificationKey"
-                       "Proving"
-                       "Verifying"
+    declare -a stages=("compile" 
+                       "witness" 
+                       "setup"
+                       "prove"
+                       "verify"
                       )
     declare -a times=("${TMP}/compiler_times.txt" 
                       "${TMP}/witness_times.txt" 
-                      "${TMP}/setup_times.txt" 
-                      "${TMP}/export_times.txt" 
+                      "${TMP}/setup_times.txt ${TMP}/export_times.txt" 
                       "${TMP}/prove_times.txt" 
                       "${TMP}/verify_times.txt" 
                      )
     arraylength=${#stages[@]}
 
+    # TODO add proof size before phyical cores
     echo "framework,category,backend,curve,circuit,input,operation,nbConstraints,nbSecret,nbPublic,ram(mb),time(ms),nbPhysicalCores,nbLogicalCores,cpu" > ${RES}
     for (( i=0; i<${arraylength}; i++ ));
     do
