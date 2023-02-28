00f015a2cc80a4ad117c3f8f9368eafbdc632e98
tumberger <jens.ernstberger@tum.de>
2023-02-28 22:03:27 +0100

    HTML styling - navbar

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ import dash_bootstrap_components as dbc
 
 
 # set app variable with dash, set external style to bootstrap theme SUPERHERO or SANDSTONE
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SANDSTONE],
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP, "/assets/style.css"],
         meta_tags=[{'name': 'viewport', 'content': 'width=device-width, initial-scale=1'},],)
 # set app server to variable for deployment
 # srv = app.server
