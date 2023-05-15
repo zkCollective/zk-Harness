cd21ef8100ecf8af6e7106f13addf02c0725ca04
tumberger <jens.ernstberger@tum.de>
2023-05-15 21:53:19 +0200

    Variable size input sha256

--- bellman_circuits/src/circuits/sha256.rs
+++ bellman_circuits/src/circuits/sha256.rs
@@ -32,12 +32,12 @@ fn sha256d<Scalar: PrimeField, CS: ConstraintSystem<Scalar>>(
         .cloned()
         .collect())
 }
-
 #[derive(Clone)]
 pub struct Sha256Circuit {
     /// The input to SHA-256d we are proving that we know. Set to `None` when we
     /// are verifying a proof (and do not have the witness data).
-    pub preimage: Option<[u8; 80]>,
+    pub preimage: Option<Vec<u8>>,
+    pub preimage_length: usize,
 }
 
 impl<Scalar: PrimeField> Circuit<Scalar> for Sha256Circuit {
@@ -46,6 +46,7 @@ impl<Scalar: PrimeField> Circuit<Scalar> for Sha256Circuit {
         // we still need to create the same constraints, so we return an equivalent-size
         // Vec of None (indicating that the value of each bit is unknown).
         let bit_values = if let Some(preimage) = self.preimage {
+            assert_eq!(preimage.len(), self.preimage_length);
             preimage
                 .into_iter()
                 .map(|byte| (0..8).map(move |i| (byte >> i) & 1u8 == 1u8))
@@ -53,9 +54,9 @@ impl<Scalar: PrimeField> Circuit<Scalar> for Sha256Circuit {
                 .map(|b| Some(b))
                 .collect()
         } else {
-            vec![None; 80 * 8]
+            vec![None; self.preimage_length * 8]
         };
-        assert_eq!(bit_values.len(), 80 * 8);
+        assert_eq!(bit_values.len(), self.preimage_length * 8);
 
         // Witness the bits of the preimage.
         let preimage_bits = bit_values
@@ -77,7 +78,6 @@ impl<Scalar: PrimeField> Circuit<Scalar> for Sha256Circuit {
     }
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -88,23 +88,31 @@ mod tests {
 
     #[test]
     fn test_sha256d_circuit() {
+        // Pick a preimage and compute its hash.
+        let hex_value = "68656c6c6f20776f726c64";
+        let preimage = hex::decode(hex_value).unwrap();
+
         // Create parameters for our circuit. In a production deployment these would
         // be generated securely using a multiparty computation.
         let params = {
-            let c = Sha256Circuit { preimage: None };
+            let c = Sha256Circuit { preimage: Some(vec![0; preimage.len()]), preimage_length: preimage.len() };
             groth16::generate_random_parameters::<Bls12, _, _>(c, &mut OsRng).unwrap()
         };
 
         // Prepare the verification key (for proof verification).
         let pvk = groth16::prepare_verifying_key(&params.vk);
 
-        // Pick a preimage and compute its hash.
-        let preimage = [42; 80];
         let hash = Sha256::digest(&Sha256::digest(&preimage));
+
+        // Convert hash result to hex string
+        let hash_hex = hex::encode(hash);
+
+        println!("SHA-256d hash: {}", hash_hex);
 
         // Create an instance of our circuit (with the preimage as a witness).
         let c = Sha256Circuit {
-            preimage: Some(preimage),
+            preimage: Some(preimage.clone()),
+            preimage_length: preimage.len(),
         };
 
         // Create a Groth16 proof with our parameters.
@@ -115,9 +123,8 @@ mod tests {
         let inputs = multipack::compute_multipacking(&hash_bits);
 
         // Check the proof!
-        // assert_eq!(groth16::verify_proof(&pvk, &proof, &inputs), Ok(true));
-        // Check the proof!
         assert!(groth16::verify_proof(&pvk, &proof, &inputs).is_ok());
     }
 }
+
 
