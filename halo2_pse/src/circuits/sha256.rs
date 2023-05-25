afadc57ec8809a9d91a0e8097d6977aec8a5a743
Stefanos Chaliasos <stefanos@chaliasos.com>
2023-05-25 13:06:23 +0100

    Update sha256 code and use hardcoded values
    We should update it to work with any input

--- halo2_pse/src/circuits/sha256.rs
+++ halo2_pse/src/circuits/sha256.rs
@@ -13,7 +13,8 @@ use serde_json;
 
 #[derive(Default, Clone)]
 pub struct Sha256Circuit {
-    pub sha_data: Vec<BlockWord>,
+    // FIXME that should be Vec<BlockWord>
+    pub sha_data: usize,
 }
 
 impl Circuit<Fr> for Sha256Circuit {
@@ -35,8 +36,49 @@ impl Circuit<Fr> for Sha256Circuit {
     ) -> Result<(), Error> {
         Table16Chip::load(config.clone(), &mut layouter)?;
         let table16_chip = Table16Chip::construct(config);
-        Sha256::digest(table16_chip.clone(), layouter.namespace(|| "'sha one'"),
-            self.sha_data.clone().into_boxed_slice().as_ref().try_into().unwrap());
+
+        // FIXME Remove that code
+        match self.sha_data {
+            0..=64 => Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
+                &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 2]
+            ).unwrap(),
+            65..=128 => Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
+                &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 3]
+            ).unwrap(),
+            129..=256 => Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
+                &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 5]
+            ).unwrap(),
+            257..=512 => Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
+                &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 9]
+            ).unwrap(),
+            513..=1024 => Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
+                &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 17]
+            ).unwrap(),
+            1025..=2048 => Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
+                &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 33]
+            ).unwrap(),
+            2049..=4096 => Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
+                &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 65]
+            ).unwrap(),
+            4097..=8192 => Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
+                &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 129]
+            ).unwrap(),
+            8193..=16384 => Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
+                &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 257]
+            ).unwrap(),
+            16385..=32768 => Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
+                &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 513]
+            ).unwrap(),
+            32769..=65536 => Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
+                &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 1025]
+            ).unwrap(),
+            _ => panic!("unexpected sha data"),
+        };
+        
+        // Should be as following:
+        // 
+        //Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
+        //    self.sha_data.clone().into_boxed_slice().as_ref().try_into().unwrap()).unwrap();
         Ok(())
     }
 }
@@ -69,28 +111,25 @@ pub struct Sha256Data {
 
 pub fn get_sha256_data (
     input_file_str: String
-) -> (u32, Vec<BlockWord> ){
+) -> (u32, usize ){
+    // FIXME this function should return Vec<BlockWord>
     let data: Sha256Data = serde_json::from_str(&input_file_str).expect("Cannot read json string");
     let preimage_length: usize = data.PreImage.len();
     println!("Preimage length {}", preimage_length);
     let k = match preimage_length {
-        0..=64 => 17,
-        65..=128 => 17,
-        129..=256 => 17,
-        257..=512 => 17,
-        513..=1024 => 17,
-        1025..=2048 => 17,
-        2049..=4096 => 17,
-        4097..=8192 => 17,
-        8193..=16384 => 17,
-        16385..=32768 => 17,
-        32769..=65536 => 17,
+        0..=2048 => 17,
+        2049..=4096 => 18,
+        4097..=8192 => 19,
+        8193..=16384 => 20,
+        16385..=32768 => 21,
+        32769..=65536 => 22,
         _ => 1,
     };
     let preimage = data.PreImage;
     let _hash = data.Hash;
-    let sha_data = hex_to_binary_words(&preimage);
-    return (k, sha_data);
+    // FIXME fix that function
+    //let sha_data = hex_to_binary_words(&preimage);
+    return (k, preimage_length);
 }
 
 #[cfg(test)]
