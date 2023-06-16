6edf17c0b0fb77ede73792ae3c1e715a8af28cf5
Stefanos Chaliasos <stefanos@chaliasos.com>
2023-06-16 17:49:47 +0300

    Use sha256 inputs dynamically

--- halo2_pse/src/circuits/sha256.rs
+++ halo2_pse/src/circuits/sha256.rs
@@ -14,7 +14,7 @@ use serde_json;
 #[derive(Default, Clone)]
 pub struct Sha256Circuit {
     // FIXME that should be Vec<BlockWord>
-    pub sha_data: usize,
+    pub sha_data: Vec<BlockWord>,
 }
 
 impl Circuit<Fr> for Sha256Circuit {
@@ -38,69 +38,76 @@ impl Circuit<Fr> for Sha256Circuit {
         let table16_chip = Table16Chip::construct(config);
 
         // FIXME Remove that code
-        match self.sha_data {
-            0..=64 => Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
-                &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 2]
-            ).unwrap(),
-            65..=128 => Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
-                &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 3]
-            ).unwrap(),
-            129..=256 => Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
-                &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 5]
-            ).unwrap(),
-            257..=512 => Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
-                &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 9]
-            ).unwrap(),
-            513..=1024 => Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
-                &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 17]
-            ).unwrap(),
-            1025..=2048 => Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
-                &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 33]
-            ).unwrap(),
-            2049..=4096 => Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
-                &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 65]
-            ).unwrap(),
-            4097..=8192 => Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
-                &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 129]
-            ).unwrap(),
-            8193..=16384 => Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
-                &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 257]
-            ).unwrap(),
-            16385..=32768 => Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
-                &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 513]
-            ).unwrap(),
-            32769..=65536 => Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
-                &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 1025]
-            ).unwrap(),
-            _ => panic!("unexpected sha data"),
-        };
+        //match self.sha_data {
+        //    0..=64 => 
+        //        Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
+        //        &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16]
+        //    ).unwrap(),
+        //    65..=128 => Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
+        //        &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 3]
+        //    ).unwrap(),
+        //    129..=256 => Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
+        //        &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 5]
+        //    ).unwrap(),
+        //    257..=512 => Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
+        //        &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 9]
+        //    ).unwrap(),
+        //    513..=1024 => Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
+        //        &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 17]
+        //    ).unwrap(),
+        //    1025..=2048 => Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
+        //        &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 33]
+        //    ).unwrap(),
+        //    2049..=4096 => Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
+        //        &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 65]
+        //    ).unwrap(),
+        //    4097..=8192 => Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
+        //        &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 129]
+        //    ).unwrap(),
+        //    8193..=16384 => Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
+        //        &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 257]
+        //    ).unwrap(),
+        //    16385..=32768 => Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
+        //        &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 513]
+        //    ).unwrap(),
+        //    32769..=65536 => Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
+        //        &[BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 1025]
+        //    ).unwrap(),
+        //    _ => panic!("unexpected sha data"),
+        //};
         
         // Should be as following:
         // 
-        //Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
-        //    self.sha_data.clone().into_boxed_slice().as_ref().try_into().unwrap()).unwrap();
+        Sha256::digest(table16_chip, layouter.namespace(|| "'sha one'"),
+            self.sha_data.clone().into_boxed_slice().as_ref().try_into().unwrap()).unwrap();
         Ok(())
     }
 }
 
 fn hex_to_binary_words(input: &str) -> Vec<BlockWord> {
-    let bytes = hex::decode(input).expect("Invalid hex input");
-    let binary_string = bytes.iter().map(|byte| format!("{:08b}", byte)).collect::<String>();
+    let mut block_words: Vec<BlockWord> = Vec::new();
 
-    let padded_binary_string = format!("{:0>width$}", binary_string, width = (binary_string.len() + 31) / 32 * 32);
+    // Convert the input string to bytes
+    let bytes_data = hex::decode(input).unwrap();
 
-    let words: Vec<BlockWord> = padded_binary_string
-        .chars()
-        .collect::<Vec<char>>()
-        .chunks(32)
-        .map(|chunk| {
-            let word_str: String = chunk.iter().collect();
-            let value = u32::from_str_radix(&word_str, 2).unwrap();
-            BlockWord(Value::known(value))
-        })
-        .collect();
-    println!("words length {:?}", words.len());
-    words
+    // Split the bytes into chunks of 4 bytes (32 bits)
+    let chunk_size = 4;
+    for chunk in bytes_data.chunks_exact(chunk_size) {
+        // Create the u32 value from the chunk
+        let value = u32::from_be_bytes(chunk.try_into().unwrap());
+
+        // Create the BlockWord and push it to the vector
+        let block_word = BlockWord(Value::known(value));
+        block_words.push(block_word);
+    }
+    if block_words.len() < 16 {
+        println!("Input is less than 16 words, padding with zeros is required");
+        let padding = 16 - block_words.len();
+        for _ in 0..padding {
+            block_words.push(BlockWord(Value::known(0)));
+        }
+    }
+    block_words
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -111,8 +118,7 @@ pub struct Sha256Data {
 
 pub fn get_sha256_data (
     input_file_str: String
-) -> (u32, usize ){
-    // FIXME this function should return Vec<BlockWord>
+) -> (u32, Vec<BlockWord>){
     let data: Sha256Data = serde_json::from_str(&input_file_str).expect("Cannot read json string");
     let preimage_length: usize = data.PreImage.len();
     println!("Preimage length {}", preimage_length);
@@ -128,8 +134,8 @@ pub fn get_sha256_data (
     let preimage = data.PreImage;
     let _hash = data.Hash;
     // FIXME fix that function
-    //let sha_data = hex_to_binary_words(&preimage);
-    return (k, preimage_length);
+    let sha_data = hex_to_binary_words(&preimage);
+    return (k, sha_data);
 }
 
 #[cfg(test)]
@@ -141,8 +147,7 @@ mod tests {
     #[test]
     fn test_exponentiation_circuit() {
         use halo2_proofs::dev::MockProver;
-        let k = 17;
-        let sha_data = vec![BlockWord(Value::known(0b01111000100000000000000000000000)); 16 * 1];
+        let (k, sha_data) = get_sha256_data(String::from("{ \"PreImage\": \"28ca152c94e1db7f7d892d27b5b674dd414028635c3c0321289f9afb0eee906a\", \"Hash\": \"c80263908bbc7bece8d340575547cf920206178f36f37ea787abe81ef2e24043\" }"));
         let circuit = Sha256Circuit {
             sha_data: sha_data,
         };
