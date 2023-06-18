57f8de08d0dd645d3d964abc61f2a71af02a7ac1
tumberger <jens.ernstberger@tum.de>
2023-06-18 16:47:25 -0700

    Update output format gnark

--- gnark/util/output.go
+++ gnark/util/output.go
@@ -2,8 +2,6 @@ package util
 
 import (
 	"strconv"
-
-	. "github.com/klauspost/cpuid/v2"
 )
 
 type HeadersProvider interface {
@@ -12,70 +10,6 @@ type HeadersProvider interface {
 
 type ValuesProvider interface {
 	Values() []string
-}
-
-type BenchDataArithmetic struct {
-	Framework string
-	Category  string
-	Curve     string
-	Field     string
-	Operation string
-	Input     string
-	MaxRAM    uint64
-	Count     int
-	RunTime   int64
-}
-
-func (bDataArith BenchDataArithmetic) Headers() []string {
-	return []string{"framework", "category", "curve", "field", "operation", "input", "ram", "time(ns)", "nbPhysicalCores", "nbLogicalCores", "count", "cpu"}
-}
-
-func (bDataArith BenchDataArithmetic) Values() []string {
-	return []string{
-		bDataArith.Framework,
-		bDataArith.Category,
-		bDataArith.Curve,
-		bDataArith.Field,
-		bDataArith.Operation,
-		bDataArith.Input,
-		strconv.Itoa(int(bDataArith.MaxRAM)),
-		strconv.Itoa(int(bDataArith.RunTime)),
-		strconv.Itoa(CPU.PhysicalCores),
-		strconv.Itoa(CPU.LogicalCores),
-		strconv.Itoa(int(bDataArith.Count)),
-		CPU.BrandName,
-	}
-}
-
-type BenchDataCurve struct {
-	Framework string
-	Category  string
-	Curve     string
-	Operation string
-	Input     string
-	MaxRAM    uint64
-	Count     int
-	RunTime   int64
-}
-
-func (bDataCurve BenchDataCurve) Headers() []string {
-	return []string{"framework", "category", "curve", "operation", "input", "ram", "time(ms)", "nbPhysicalCores", "nbLogicalCores", "count", "cpu"}
-}
-
-func (bDataCurve BenchDataCurve) Values() []string {
-	return []string{
-		bDataCurve.Framework,
-		bDataCurve.Category,
-		bDataCurve.Curve,
-		bDataCurve.Operation,
-		bDataCurve.Input,
-		strconv.Itoa(int(bDataCurve.MaxRAM)),
-		strconv.Itoa(int(bDataCurve.RunTime)),
-		strconv.Itoa(CPU.PhysicalCores),
-		strconv.Itoa(CPU.LogicalCores),
-		strconv.Itoa(int(bDataCurve.Count)),
-		CPU.BrandName,
-	}
 }
 
 type BenchDataCircuit struct {
@@ -96,7 +30,7 @@ type BenchDataCircuit struct {
 }
 
 func (bDataCirc BenchDataCircuit) Headers() []string {
-	return []string{"framework", "category", "backend", "curve", "circuit", "input", "operation", "nbConstraints", "nbSecret", "nbPublic", "ram", "time(ms)", "proofSize", "nbPhysicalCores", "nbLogicalCores", "count", "cpu"}
+	return []string{"framework", "category", "backend", "curve", "circuit", "input", "operation", "nbConstraints", "nbSecret", "nbPublic", "ram", "time(ms)", "proofSize", "count"}
 }
 
 func (bDataCirc BenchDataCircuit) Values() []string {
@@ -114,10 +48,7 @@ func (bDataCirc BenchDataCircuit) Values() []string {
 		strconv.Itoa(int(bDataCirc.MaxRAM)),
 		strconv.Itoa(int(bDataCirc.RunTime)),
 		strconv.Itoa(int(bDataCirc.ProofSize)),
-		strconv.Itoa(CPU.PhysicalCores),
-		strconv.Itoa(CPU.LogicalCores),
 		strconv.Itoa(int(bDataCirc.Count)),
-		CPU.BrandName,
 	}
 }
 
@@ -136,13 +67,13 @@ type BenchDataRecursion struct {
 	NbSecretVariables  int
 	NbPublicVariables  int
 	MaxRAM             uint64
-	Count              int
 	RunTime            int64
 	ProofSize          int
+	Count              int
 }
 
 func (bDataCirc BenchDataRecursion) Headers() []string {
-	return []string{"framework", "category", "innerBackend", "outerBackend", "innerCurve", "outerCurve", "circuit", "input", "operation", "innerNbConstraints", "outerNbConstraints", "nbSecret", "nbPublic", "ram", "time(ms)", "proofSize", "nbPhysicalCores", "nbLogicalCores", "count", "cpu"}
+	return []string{"framework", "category", "innerBackend", "outerBackend", "innerCurve", "outerCurve", "circuit", "input", "operation", "innerNbConstraints", "outerNbConstraints", "nbSecret", "nbPublic", "ram", "time(ms)", "proofSize", "count"}
 }
 
 func (bDataCirc BenchDataRecursion) Values() []string {
@@ -163,9 +94,6 @@ func (bDataCirc BenchDataRecursion) Values() []string {
 		strconv.Itoa(int(bDataCirc.MaxRAM)),
 		strconv.Itoa(int(bDataCirc.RunTime)),
 		strconv.Itoa(int(bDataCirc.ProofSize)),
-		strconv.Itoa(CPU.PhysicalCores),
-		strconv.Itoa(CPU.LogicalCores),
 		strconv.Itoa(int(bDataCirc.Count)),
-		CPU.BrandName,
 	}
 }
