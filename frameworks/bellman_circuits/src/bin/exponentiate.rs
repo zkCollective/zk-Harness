f8fbad494cf66e6d75dd525e8c06962071a51719
tumberger <jens.ernstberger@tum.de>
2023-06-19 11:31:49 -0700

    fix bellman memory verify

--- frameworks/bellman_circuits/src/bin/exponentiate.rs
+++ frameworks/bellman_circuits/src/bin/exponentiate.rs
@@ -13,11 +13,10 @@ fn main() {
     let input_str = read_file_contents(args.input);
 
     // Get data from config
-    let (x_64, e, y_64) = exponentiate::get_exponentiate_data(input_str);
+    let (x, e, y) = exponentiate::get_exponentiate_data(input_str);
 
-    // Create Scalar from some values
-    let x = Scalar::from(x_64);
-    let y = Scalar::from(y_64);
+    // Public input is empty
+    let inputs = Vec::new();
 
     if args.phase == "setup" {
         let circuit = exponentiate::ExponentiationCircuit {
@@ -38,9 +37,6 @@ fn main() {
         f_prove(circuit, params_file, proof_file);
     } else if args.phase == "verify" {
         // Public inputs are x and y
-        let x_bits = multipack::bytes_to_bits_le(&x.to_repr().as_ref());
-        let y_bits = multipack::bytes_to_bits_le(&y.to_repr().as_ref());
-        let inputs: Vec<Scalar> = [multipack::compute_multipacking(&x_bits), multipack::compute_multipacking(&y_bits)].concat();
         let params_file = args.params.expect("Missing params argument");
         let proof_file = args.proof.expect("Missing proof argument");
         f_verify(params_file, proof_file, inputs)
